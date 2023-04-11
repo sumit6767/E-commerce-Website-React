9fb9f363851552c7d7648e6e1bbb22fc20d62557
sumit <sumitvishwakarma6767@gmail.com>
2023-04-11 19:40:17 +0530

    add button for item increment added

--- src/Components/Meals/MealItem.js
+++ src/Components/Meals/MealItem.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import style from './Meals.module.css'
 
 const MealItem = (props)=>{
+    const [count,setCount] = useState(1);
+    function increment(){
+        setCount(prev=>{
+            return prev+1
+        });
+    }
+    function submitHandler(e){
+        e.preventDefault()
+        increment()
+    }
     return(
-        <div>
+        <>
+        <div className={style.item}>
             <p>{props.dishName}</p>
             <p style={{fontStyle:"italic"}}>{props.dishTitle}</p>
             <p style={{fontWeight:"bolder",color:"darkred"}}>&#x20B9;{props.price}</p>
-            <p>--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+            <form onSubmit={submitHandler} className={style.amountCount}>
+                <span>Amount</span><input type="number"  className={style.box}  value={count}/> 
+                <Button className={style.button}>+ Add</Button>
+            </form>
         </div>
+        <p>--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+        </>
     )
 }
 export default MealItem
